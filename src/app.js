9a34a86f9b080f7152631a3853fbd01906f5c908
RicardoAli <61981276+RicardoAli@users.noreply.github.com>
2025-11-02 02:35:50 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+//window.onload 
+let generateCard = document.getElementById("generateButton");
+generateCard.onclick = function () {
   console.log("Hello Rigo from the console!");
-};
+  let numberBody = document.getElementById("numberBody");
+  let numberList = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let indexList = Math.floor(Math.random() * numberList.length);
+  numberBody.textContent = numberList[indexList];
+  console.log(numberList[indexList]);
+
+
+  let paloHeader = document.getElementById("paloHeader");
+  let paloFooter = document.getElementById("paloFooter");
+  let paloList = ["♦", "♥", "♠", "♣"];
+  let indexPalo = Math.floor(Math.random() * paloList.length);
+  paloHeader.textContent = paloList[indexPalo];
+  paloFooter.textContent = paloList[indexPalo];
+  if (paloList[indexPalo] === "♦" || paloList[indexPalo] === "♥") {
+    paloHeader.style.color = "red";
+    paloFooter.style.color = "red";
+  } else {
+    paloHeader.style.color = "black";
+    paloFooter.style.color = "black";
+  }
+  
+  };
